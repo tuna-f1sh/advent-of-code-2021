fae96100cb617db3494ba73a0252fbcc2497f062
John Whittington <git@jbrengineering.co.uk>
2021-12-06 09:41:43 +0100

    finish day 3

--- python/day3.py
+++ python/day3.py
@@ -31,27 +31,35 @@ def calculate_life_support_rating(diag):
     >>> calculate_life_support_rating(example)
     (23, 10)
     """
-    # get bit length for shift
-    bit_length = len(diag[0])
-    binary = [*map(lambda l: int(l, base=2), diag)]
+    def filter_scrubbers(binary_strs: list, function):
+        bit_length = len(binary_strs[0])
+        binary_ints = {*map(lambda l: int(l, base=2), set(binary_strs))}
 
-    ones_count = [0] * bit_length
+        for bi in range(bit_length-1, -1, -1):
+            leading_1 = set(filter(lambda x: x & (1 << bi) == (0x01 << bi), binary_ints))
+            leading_0 = binary_ints - leading_1
 
-    for b in binary:
-        for bit in range(bit_length):
-            ones_count[bit] += (b >> bit) & 0x01
+            if function(len(leading_0), len(leading_1)):
+                binary_ints = leading_1
+            else:
+                binary_ints = leading_0
 
-    gamma_rate = 0
-    epsilon_rate = 0
+            if len(binary_ints) == 1:
+                return binary_ints.pop()
 
-    for i, b in enumerate(map(lambda r: r > len(diag)/2, ones_count)):
-        gamma_rate += 1 << i if b else 0
-        epsilon_rate += 1 << i if not b else 0
+        # not found
+        return 0
 
-    return gamma_rate, epsilon_rate
+    # 1s greater than 0s
+    oxygen_rating = filter_scrubbers(diag, lambda x, y: x <= y)
+    # 0s greater than 1s
+    carbon_dioxide_rating = filter_scrubbers(diag, lambda x, y: x > y)
+
+    return oxygen_rating, carbon_dioxide_rating
 
 dinput = get_input(3)
 part1 = calculate_power_consumption(dinput)
+part2 = calculate_life_support_rating(dinput)
 
 print(f"Part 1 result: {part1[0] * part1[1]}")
-# print(f"Part 2 result: {sub2.horizontal * sub2.depth}")
+print(f"Part 2 result: {part2[0] * part2[1]}")
